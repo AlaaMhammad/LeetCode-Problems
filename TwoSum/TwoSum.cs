594b33bb592bdfa0a0b542897c522598d7d32bbe
Alaa Eldin Mohammed  Al-Dohdar <144788318+AlaaMhammad@users.noreply.github.com>
2025-01-14 08:59:58 +0200

    Add files via upload

---
+++ TwoSum/TwoSum.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> numMap = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i];
+            if (numMap.ContainsKey(complement)) {
+                return new int[] { numMap[complement], i };
+            }
+            if (!numMap.ContainsKey(nums[i])) {
+                numMap[nums[i]] = i;
+            }
+        }
+        throw new ArgumentException("No solution found");
+    }
+}
